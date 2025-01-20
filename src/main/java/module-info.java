e76de9b100fe481f674033503667bb69022670d5
Becanar <benatortubitarte@gmail.com>
2025-01-20 12:35:45 +0100

    Añado recursos

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,12 @@
 module com.benat.cano.jasperej2 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires net.sf.jasperreports.core;
 
 
-    opens com.benat.cano.jasperej2 to javafx.fxml;
-    exports com.benat.cano.jasperej2;
+    exports com.benat.cano.jasperej2.controller;
+    opens com.benat.cano.jasperej2.controller to javafx.fxml;
+    exports com.benat.cano.jasperej2.app;
+    opens com.benat.cano.jasperej2.app to javafx.fxml;
 }
